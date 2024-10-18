3cd50260c567d55cba0e655ccf9e88671389182f
Henrykim1111 <henrykim9319@gmail.com>
2024-10-18 00:29:00 -0400

    BN1 created

--- sleep-quality.R
+++ sleep-quality.R
@@ -5,6 +5,7 @@ library(bnlearn) # bnlearn
 library(ggplot2) # visualization
 library(VIM) # kNN
 library(lubridate) # change date
+library(dplyr) # data manipulation
 
 data <- read.csv(file='Sleep_Efficiency.csv', header=TRUE, sep=",", na.strings=".")
 original_data <- data
@@ -14,25 +15,48 @@ head(data)
 
 # Preprocessing
 ## Feature encoding
+### Age
+data$Age <- as.factor(data$Age)
+
 ### Gender
 data$Gender <- as.factor(data$Gender)
 
 ### Bedtime
 data$Bedtime <- ymd_hms(data$Bedtime)
 data$Bedtime <- format(data$Bedtime, "%H:%M")
+data$Bedtime <- as.factor(data$Bedtime)
 
 ### Wakeup.time
 data$Wakeup.time <- ymd_hms(data$Wakeup.time)
 data$Wakeup.time <- format(data$Wakeup.time, "%H:%M")
+data$Wakeup.time <- as.factor(data$Wakeup.time)
 
-### Smoking.status
-data$Smoking.status <- as.factor(data$Smoking.status)
+### Sleep duration
+data$Sleep.duration <- as.factor(data$Sleep.duration)
+
+### Sleep efficiency
+data$Sleep.efficiency <- as.numeric(data$Sleep.efficiency)
+
+### Rem sleep percentage
+data$REM.sleep.percentage <- as.numeric(data$REM.sleep.percentage)
+
+### Deep sleep percentage
+data$Deep.sleep.percentage <- as.numeric(data$Deep.sleep.percentage)
+
+### Light sleep percentage
+data$Light.sleep.percentage <- as.numeric(data$Light.sleep.percentage)
+
+### Awakenings
+data$Awakenings <- as.factor(data$Awakenings)
 
 ### Caffein.consumption
 data$Caffeine.consumption <- as.numeric(data$Caffeine.consumption)
 
 ### Alcohol.consumption
 data$Alcohol.consumption <- as.numeric(data$Alcohol.consumption)
+
+### Smoking.status
+data$Smoking.status <- as.factor(data$Smoking.status)
 
 ### Exercise.frequency
 data$Exercise.frequency <- as.numeric(data$Exercise.frequency)
@@ -48,21 +72,22 @@ na_counts[na_counts > 0]
 ### Exercise.frequency : 6
 imputed_data <- kNN(data)
 
-any(is.na(imputed_data))
-
 ### changed data
 awakenings_imputed <- imputed_data[imputed_data$Awakenings_imp == TRUE, "Awakenings"]
 caffeine_imputed <- imputed_data[imputed_data$Caffeine.consumption_imp == TRUE, "Caffeine.consumption"]
 alcohol_imputed <- imputed_data[imputed_data$Alcohol.consumption_imp == TRUE, "Alcohol.consumption"]
 exercise_imputed <- imputed_data[imputed_data$Exercise.frequency_imp == TRUE, "Exercise.frequency"]
 
+imputed_data_filtered <- imputed_data %>% select(-(16:30))
+any(is.na(imputed_data_filtered))
+
 ### Plot to compare
 imputed_columns <- c("Awakenings", "Caffeine.consumption", "Alcohol.consumption", "Exercise.frequency")
 
 for (col in imputed_columns) {
   original_data_long <- data.frame(Value = original_data[[col]], Dataset = "Original")
-  imputed_data_long <- data.frame(Value = imputed_data[[col]], Dataset = "Imputed")
-  combined_data <- rbind(original_data_long, imputed_data_long)
+  imputed_data_filtered_long <- data.frame(Value = imputed_data_filtered[[col]], Dataset = "Imputed")
+  combined_data <- rbind(original_data_long, imputed_data_filtered_long)
   
   p <- ggplot(combined_data, aes(x = Value, fill = Dataset)) +
     geom_density(alpha = 0.5) +
@@ -73,3 +98,8 @@ for (col in imputed_columns) {
 }
 
 
+bn_df <- data.frame(imputed_data_filtered)
+bn_df <- bn_df %>% select(-ID)
+head(bn_df)
+res <- hc(bn_df)
+plot(res)
