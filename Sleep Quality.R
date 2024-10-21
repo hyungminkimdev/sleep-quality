99536113f0fcf425993dff42748ad50df30d3695
Henrykim1111 <henrykim9319@gmail.com>
2024-10-21 02:16:47 -0400

    discretization done

--- Sleep Quality.R
+++ Sleep Quality.R
@@ -40,38 +40,22 @@ data_1$Bedtime <- as.factor(data_1$Bedtime)
 data_1$Wake.up.Time <- as.factor(data_1$Wake.up.Time)
 
 # Daily Steps
-data_1$Daily.Steps <- as.factor()
+data_1$Daily.Steps <- as.numeric(data_1$Daily.Steps)
 
+# Calories.Burned
+data_1$Calories.Burned <- as.numeric(data_1$Calories.Burned)
 
+# Physical.Activity.Level
+data_1$Physical.Activity.Level <- as.factor((data_1$Physical.Activity.Level))
 
+# Dietary.Habits
+data_1$Dietary.Habits <- as.factor(data_1$Dietary.Habits)
 
+# Sleep.Disorders
+data_1$Sleep.Disorders <- as.factor(data_1$Sleep.Disorders)
 
-# Sleep efficiency
-data_1$Sleep.efficiency <- as.numeric(data_1$Sleep.efficiency)
-
-# Rem sleep percentage
-data_1$REM.sleep.percentage <- as.numeric(data_1$REM.sleep.percentage)
-
-# Deep sleep percentage
-data_1$Deep.sleep.percentage <- as.numeric(data_1$Deep.sleep.percentage)
-
-# Light sleep percentage
-data_1$Light.sleep.percentage <- as.numeric(data_1$Light.sleep.percentage)
-
-# Awakenings
-data_1$Awakenings <- as.factor(data_1$Awakenings)
-
-# Caffein.consumption
-data_1$Caffeine.consumption <- factor(data_1$Caffeine.consumption)
-
-# Alcohol.consumption
-data_1$Alcohol.consumption <- factor(data_1$Alcohol.consumption)
-
-# Smoking.status
-data_1$Smoking.status <- as.factor(data_1$Smoking.status)
-
-# Exercise.frequency
-data_1$Exercise.frequency <- as.factor(data_1$Exercise.frequency)
+# Medication.Usage
+data_1$Medication.Usage <- as.factor((data_1$Medication.Usage))
 
 
 # Missing Values
@@ -80,75 +64,90 @@ na_counts[na_counts > 0]
 
 
 # Fit BN1
-bn_1_df <- data.frame(data_1_filtered)
-bn_1_df <- bn_1_df %>% select(-ID)
+bn_1_df <- data.frame(data_1)
+bn_1_df <- bn_1_df %>% select(-User.ID)
 bn_1 <- hc(bn_1_df)
 plot(bn_1)
 graphviz.plot(bn_1, layout = "dot")
 
 
 
-
-
-
 # Discretize continuous variables
-
 # Age
-data_1 <- data_1 %>% mutate(Age.group = cut(Age, 
+data_2 <- data_1
+data_2 <- data_2 %>% mutate(Age.group = cut(Age, 
                                             breaks = seq(0, 100, by = 10), 
                                             right = FALSE, 
                                             labels = paste(seq(0, 90, by = 10), seq(9, 99, by = 10), sep = "-")))
-data_1 <- data_1 %>% select(-Age)
-data_1$Age.group <- as.factor(data_1$Age.group)
+data_2 <- data_2 %>% select(-Age)
+data_2$Age.group <- as.factor(data_2$Age.group)
+
+# Fit BN2
+bn_2_df <- data.frame(data_2)
+bn_2_df <- bn_2_df %>% select(-User.ID)
+bn_2 <- hc(bn_2_df)
+plot(bn_2)
+graphviz.plot(bn_2, layout = "dot")
+
 
 # Sleep duration
-data_1 <- data_1 %>% mutate(Bedtime = as.POSIXct(Bedtime, format = "%H:%M"),
+data_3 <- data_2
+data_3 <- data_3 %>% mutate(Bedtime = as.POSIXct(Bedtime, format = "%H:%M"),
                             Wake.up.Time = as.POSIXct(Wake.up.Time, format = "%H:%M"),
                             Sleep.duration = ifelse(Wake.up.Time < Bedtime, 
                                                     as.numeric(difftime(Wake.up.Time + 86400, Bedtime, units = "hours")), 
                                                     as.numeric(difftime(Wake.up.Time, Bedtime, units = "hours"))))
 
-data_1 <- data_1 %>% mutate(Sleep.duration.group = cut(Sleep.duration, 
+data_3 <- data_3 %>% mutate(Sleep.duration.group = cut(Sleep.duration, 
                                                        breaks = seq(3, 10, by = 0.5), 
                                                        right = FALSE, 
                                                        labels = paste(seq(3, 8.5, by = 0.5), seq(3.5, 10, by = 0.5), sep = "-")))
-data_1 <- data_1 %>% select(-Sleep.duration)
+data_3 <- data_3 %>% select(-Sleep.duration)
 
-data_1$Wake.up.Time <- ymd_hms(data_1$Wake.up.Time)
-data_1$Wake.up.Time <- format(data_1$Wake.up.Time, "%H:%M")
+data_3$Wake.up.Time <- ymd_hms(data_3$Wake.up.Time)
+data_3$Wake.up.Time <- format(data_3$Wake.up.Time, "%H:%M")
 
-data_1$Bedtime <- ymd_hms(data_1$Bedtime)
-data_1$Bedtime <- format(data_1$Bedtime, "%H:%M")
+data_3$Bedtime <- ymd_hms(data_3$Bedtime)
+data_3$Bedtime <- format(data_3$Bedtime, "%H:%M")
+
+data_3$Bedtime <- as.factor(data_3$Bedtime)
+data_3$Wake.up.Time <- as.factor(data_3$Wake.up.Time)
+data_3$Sleep.duration.group <- as.factor(data_3$Sleep.duration.group)
+
+# Fit BN3
+bn_3_df <- data.frame(data_3)
+bn_3_df <- bn_3_df %>% select(-User.ID)
+bn_3 <- hc(bn_3_df)
+plot(bn_3)
+graphviz.plot(bn_3, layout = "dot")
 
 
-data_1$Sleep.duration.group <- as.factor(data_1$Sleep.duration.group)
+# Daily.Steps and Calories.Burned
+data_4 <- data_3
+continuous_vars <- c("Daily.Steps", "Calories.Burned")
+data_4_cols_discretized <- discretize(data_4, method = "hartemink", breaks = 3, ordered = TRUE, data = data_4[, continuous_vars])
+table(data_4_cols_discretized$Daily.Steps)
+table(data_4_cols_discretized$Calories.Burned)
+
+temp <- data_4
+temp[, continuous_vars] <- data_4_cols_discretized
+data_4_discretized <- temp
+head(data_4_discretized)
+
+# Fit BN4
+bn_4_df <- data.frame(data_4_discretized)
+bn_4_df <- bn_4_df %>% select(-User.ID)
+bn_4 <- hc(bn_4_df)
+plot(bn_4)
+graphviz.plot(bn_4, layout = "dot")
+
+
+# Evaluate Model
+bic_score_bn_4 <- score(bn_4, data = bn_4_df, type = "bic")
+print(paste("BIC Score:", bic_score_bn_4))
 
 
 
-
-summary(data_1$Age.group)
-
-
-
-continuous_vars <- c("Sleep.efficiency", "REM.sleep.percentage", "Deep.sleep.percentage", "Light.sleep.percentage")
-data_1_cols_discretized <- discretize(data_1_filtered, method = "hartemink", breaks = 3, ordered = TRUE, data = data_1_filtered[, continuous_vars])
-head(data_1_cols_discretized)
-table(data_1_cols_discretized$Sleep.efficiency)
-table(data_1_cols_discretized$REM.sleep.percentage)
-table(data_1_cols_discretized$Deep.sleep.percentage)
-table(data_1_cols_discretized$Light.sleep.percentage)
-
-temp <- data_1_filtered
-temp[, continuous_vars] <- data_1_cols_discretized
-data_1_discretized <- temp
-head(data_1_discretized)
-
-# Fit BN2
-bn_2_df <- data.frame(data_1_discretized)
-bn_2_df <- bn_2_df %>% select(-ID)
-bn_2 <- hc(bn_2_df)
-plot(bn_2)
-graphviz.plot(bn_2, layout = "dot")
 
 # Plot using graphviz
 fitted_bn_2 <- bn.fit(bn_2, data = bn_2_df)
@@ -163,9 +162,6 @@ graphviz.chart(
 )
 fitted_bn_2
 
-# Evaluate Model
-bic_score_bn_2 <- score(bn_2, data = bn_2_df, type = "bic")
-print(paste("BIC Score:", bic_score_bn_2))
 
 
 
