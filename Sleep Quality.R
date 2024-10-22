cfec985b0a829ebf19c2047e9e0d28dda7922238
Henrykim1111 <henrykim9319@gmail.com>
2024-10-22 07:25:51 -0400

    P1 done

--- Sleep Quality.R
+++ Sleep Quality.R
@@ -4,7 +4,10 @@ setwd("/Users/henry/Desktop/Virginia Tech/2024 FALL/2024 Fall Lectures/Emerging 
 install.packages(c("bnlearn", "ggplot2", "VIM", "lubridate", "dplyr", "BiocManager"))
 BiocManager::install("Rgraphviz")
 install.packages("gRain")
-
+install.packages("infotheo")
+install.packages("reshape2")
+install.packages("caret")
+install.packages("pdp")
 
 # Import Libraries
 library(bnlearn) # bnlearn
@@ -14,14 +17,17 @@ library(lubridate) # change date
 library(dplyr) # data manipulation
 library(Rgraphviz) # rgraphviz
 library(gRain)
-
+library(infotheo) # Calculate Mutual Information (MI)
+library(reshape2) # for melt MI
+library(caret) # Effectiveness
+library(pdp) # Sensitivity Analysis
 
 original_data <- read.csv(file='Health_Sleep_Statistics.csv', header=TRUE, sep=",", na.strings=".")
 data_1 <- original_data
 summary(data_1)
 # View(data_1)
 head(data_1)
-
+colnames(data_1)
 
 # Preprocessing
 # Age
@@ -62,14 +68,58 @@ data_1$Medication.Usage <- as.factor((data_1$Medication.Usage))
 na_counts <- colSums(is.na(data_1))
 na_counts[na_counts > 0]
 
+# MI
+mi_matrix_data_1 <- mutinformation(data_1)
+
+mi_melted_data_1 <- melt(mi_matrix_data_1)
+colnames(mi_melted_data_1) <- c("Var1", "Var2", "MI")
+ggplot(mi_melted_data_1, aes(Var1, Var2, fill = MI)) +
+  geom_tile() +
+  scale_fill_gradient2(low = "blue", mid = "white", high = "red", midpoint = median(mi_melted_data_1$MI)) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        axis.title = element_blank()) +
+  geom_text(aes(label = sprintf("%.3f", MI)), size = 3) +
+  labs(title = "Initial Pairwise Mutual Information (MI) Table")
+
+
+# Nomalized MI
+entropy_vector_data_1 <- sapply(data_1, entropy)
+
+nmi <- function(mi, ent1, ent2) {
+  return(2 * mi / (ent1 + ent2))
+}
+
+nmi_matrix_data_1 <- outer(
+  1:ncol(data_1),
+  1:ncol(data_1),
+  FUN = Vectorize(function(i, j) nmi(mi_matrix_data_1[i, j], entropy_vector_data_1[i], entropy_vector_data_1[j]))
+)
+
+colnames(nmi_matrix_data_1) <- rownames(nmi_matrix_data_1) <- colnames(data_1)
+
+nmi_melted_data_1 <- melt(nmi_matrix_data_1)
+colnames(nmi_melted_data_1) <- c("Var1", "Var2", "NMI")
+
+
+ggplot(nmi_melted_data_1, aes(Var1, Var2, fill = NMI)) +
+  geom_tile() +
+  scale_fill_gradient2(low = "blue", mid = "white", high = "red", midpoint = 0.5, limits = c(0, 1)) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        axis.title = element_blank()) +
+  geom_text(aes(label = sprintf("%.3f", NMI)), size = 3) +
+  labs(title = "Initial Pairwise Normalized Mutual Information (NMI) Table")
+
+
+
 
 # Fit BN1
 data_1_df <- data.frame(data_1)
 data_1_df <- data_1_df %>% select(-User.ID)
 bn_1 <- hc(data_1_df)
 plot(bn_1)
-graphviz.plot(bn_1, layout = "dot")
-
+graphviz.plot(bn_1, layout = "dot", fontsize = 20)
 
 
 # Discretize continuous variables
@@ -151,7 +201,7 @@ data_5_df <- data.frame(data_5)
 data_5_df <- data_5_df %>% select(-User.ID)
 bn_5 <- hc(data_5_df)
 plot(bn_5)
-graphviz.plot(bn_5, layout = "dot")
+graphviz.plot(bn_5, layout = "dot", fontsize = 30)
 
 
 # Evaluate BN5
@@ -217,6 +267,8 @@ new_predictions <- predict( object = bn_melatonin_model,
                             method = "bayes-lw")
 data_6$Melatonin.Level.group..pg.mL. <- new_predictions
 
+summary(data_6$Melatonin.Level.group..pg.mL.)
+
 # Fit BN6
 data_6_df <- data.frame(data_6)
 bn_6 <- hc(data_6_df)
@@ -229,23 +281,68 @@ data_7 <- kNN(data_6)
 any(is.na(data_7))
 data_7 <- data_7 %>% select(-(14:26))
 
-# 2. 불완전한 학습 모델
-# 모델이 특정 입력 값 조합에 대해 충분히 학습되지 않았거나, 추론에 필요한 변수들이 충분하지 않을 수 있습니다. Bayesian Network는 학습 데이터에 기초한 확률적 모델을 사용하는데, 일부 조합에 대한 학습이 부족하거나 해당 조합에 대한 데이터가 없을 경우 NA를 반환할 수 있습니다. (결측치나, 데이터 형식의 불일치, 데이터 불균형으로 인한 문제는 아님.)
-# 해결 방법:
-# 모델이 충분히 학습되었는지 확인하고, 필요하다면 학습 데이터를 더 많이 확보하거나 다른 변수들을 추가하여 모델을 개선할 수 있습니다.
-
 
 # Fit BN7
 data_7_df <- data.frame(data_7)
 bn_7 <- hc(data_7_df)
 plot(bn_7)
-graphviz.plot(bn_7, layout = "dot")
+graphviz.plot(bn_7, layout = "dot", fontsize = 30)
 
 
+# Mutual Information (MI)
+data_8 <- data_7
+
+mi_matrix_data_8 <- mutinformation(data_8)
+# print(mi_matrix_data_8)
+# heatmap(as.matrix(mi_matrix_data_8), Rowv=NA, Colv=NA, col=heat.colors(256), scale="column")
+
+
+mi_melted_data_8 <- melt(mi_matrix_data_8)
+colnames(mi_melted_data_8) <- c("Var1", "Var2", "MI")
+ggplot(mi_melted_data_8, aes(Var1, Var2, fill = MI)) +
+  geom_tile() +
+  scale_fill_gradient2(low = "blue", mid = "white", high = "red", midpoint = median(mi_melted_data_8$MI)) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        axis.title = element_blank()) +
+  geom_text(aes(label = sprintf("%.3f", MI)), size = 3) +
+  labs(title = "Pairwise Mutual Information (MI) Table")
+
+
+# Nomalized MI
+entropy_vector_data_8 <- sapply(data_8, entropy)
+
+nmi <- function(mi, ent1, ent2) {
+  return(2 * mi / (ent1 + ent2))
+}
+
+nmi_matrix_data_8 <- outer(
+  1:ncol(data_8),
+  1:ncol(data_8),
+  FUN = Vectorize(function(i, j) nmi(mi_matrix_data_8[i, j], entropy_vector_data_8[i], entropy_vector_data_8[j]))
+)
+
+colnames(nmi_matrix_data_8) <- rownames(nmi_matrix_data_8) <- colnames(data_8)
+
+nmi_melted_data_8 <- melt(nmi_matrix_data_8)
+colnames(nmi_melted_data_8) <- c("Var1", "Var2", "NMI")
+
+
+ggplot(nmi_melted_data_8, aes(Var1, Var2, fill = NMI)) +
+  geom_tile() +
+  scale_fill_gradient2(low = "blue", mid = "white", high = "red", midpoint = 0.5, limits = c(0, 1)) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        axis.title = element_blank()) +
+  geom_text(aes(label = sprintf("%.3f", NMI)), size = 3) +
+  labs(title = "Pairwise Normalized Mutual Information (NMI) Table")
+
+
+# Save the plot
+# ggsave("mi_heatmap.png", width = 12, height = 10, dpi = 300)
 
 
 # Whitelist, Blacklist
-data_8 <- data_7
 
 wl = matrix(c(
   "Gender","Sleep.Disorders",
@@ -266,14 +363,120 @@ bl = matrix(c(
 colnames(bl) <- c("from", "to")
 bl
 
-# Fit BN7
+# Fit BN8
 data_8_df <- data.frame(data_8)
 bn_8 <- hc(data_8_df, whitelist = wl, blacklist = bl)
 plot(bn_8)
-graphviz.plot(bn_8, layout = "dot")
+graphviz.plot(bn_8, layout = "dot", fontsize=40)
+
+# Inference Analysis
+# sample data
+set.seed(321)
+n <- nrow(data_8)
+train_index <- sample(1:n, size = 0.8 * n)
+train_data <- data_8[train_index, ]
+test_data <- data_8[-train_index, ]
+summary(train_data)
+
+# Augmentation
+set.seed(123)
+
+augment_data <- function(data, n) {
+  augmented_data <- data
+  
+  for (i in 1:n) {
+    new_entry <- data[sample(nrow(data), 1), ]
+    
+    # Continuous
+    
+    # Categorical
+    new_entry$Gender <- sample(data$Gender, 1)
+    new_entry$Physical.Activity.Level <- sample(data$Physical.Activity.Level, 1)
+    new_entry$Dietary.Habits <- sample(data$Dietary.Habits, 1)
+    new_entry$Sleep.Disorders <- sample(data$Sleep.Disorders, 1)
+    new_entry$Medication.Usage <- sample(data$Medication.Usage, 1)
+    new_entry$Age.group <- sample(data$Age.group, 1)
+    new_entry$Daily.Steps <- sample(data$Daily.Steps, 1) 
+    new_entry$Calories.Burned <- sample(data$Calories.Burned, 1)  
+    new_entry$Sleep.duration.group <- sample(data$Sleep.duration.group, 1)
+    new_entry$Sleep.Quality.Rating <- sample(data$Sleep.Quality.Rating, 1)
+    new_entry$Melatonin.Level.group..pg.mL. <- sample(data$Melatonin.Level.group..pg.mL., 1)
+    
+    augmented_data <- rbind(augmented_data, new_entry)
+  }
+  
+  return(augmented_data)
+}
+augmented_data_8 <- augment_data(train_data, 300)
+train_data <- rbind(train_data, augmented_data_8)
+summary(train_data)
 
 
 
+# fit
+bn_8_model <- bn.fit(bn_8, data = train_data)
+
+# Effectiveness
+# bn_8
+predictions_bn_8 <- predict(bn_8_model, data = test_data, node = "Sleep.Quality.Rating", method = "bayes-lw")
+
+predicted_values_bn_8 <- as.factor(predictions_bn_8)
+conf_matrix_bn_8 <- confusionMatrix(predicted_values_bn_8, as.factor(test_data$Sleep.Quality.Rating))
+accuracy_bn_8 <- conf_matrix$overall['Accuracy']
+recall_bn_8 <- conf_matrix$byClass[, 'Recall']
+precision_bn_8 <- conf_matrix$byClass[, 'Precision']
+f_score_bn_8 <- 2 * (precision * recall) / (precision + recall)
+
+cat("Accuracy: ", accuracy_bn_8, "\n")
+cat("Recall: ", recall_bn_8, "\n")
+cat("Precision: ", precision_bn_8, "\n")
+cat("F-score: ", f_score_bn_8, "\n")
 
 
 
+# Prediction using cpquery
+predicted_1 <- cpquery(bn_8_model, event = (Sleep.Quality.Rating=="1"), evidence =(Age.group == "20-29" &
+                                                                                   Gender == "m" &
+                                                                                   Dietary.Habits == "unhealthy" &
+                                                                                   Bedtime == "01:15" &
+                                                                                   Sleep.Disorders == "yes"
+                                                                                   ))
+predicted_2 <- cpquery(bn_8_model, event = (Sleep.Quality.Rating=="2"), evidence =(Age.group == "20-29" &
+                                                                                     Gender == "m" &
+                                                                                     Dietary.Habits == "unhealthy" &
+                                                                                     Bedtime == "01:15" &
+                                                                                     Sleep.Disorders == "yes"
+))
+predicted_3 <- cpquery(bn_8_model, event = (Sleep.Quality.Rating=="3"), evidence =(Age.group == "20-29" &
+                                                                                     Gender == "m" &
+                                                                                     Dietary.Habits == "unhealthy" &
+                                                                                     Bedtime == "01:15" &
+                                                                                     Sleep.Disorders == "yes"
+))
+predicted_4 <- cpquery(bn_8_model, event = (Sleep.Quality.Rating=="4"), evidence =(Age.group == "20-29" &
+                                                                                     Gender == "m" &
+                                                                                     Dietary.Habits == "unhealthy" &
+                                                                                     Bedtime == "01:15" &
+                                                                                     Sleep.Disorders == "yes"
+))
+predicted_5 <- cpquery(bn_8_model, event = (Sleep.Quality.Rating=="5"), evidence =(Age.group == "20-29" &
+                                                                                     Gender == "m" &
+                                                                                     Dietary.Habits == "unhealthy" &
+                                                                                     Bedtime == "01:15" &
+                                                                                     Sleep.Disorders == "yes"
+))
+
+predicted_1
+predicted_2
+predicted_3
+predicted_4
+predicted_5
+
+bn_8_model <- bn.fit(bn_8, data = data_8)
+predicted <- cpquery(bn_8_model, event = (Sleep.Quality.Rating=="4"), evidence =(Age.group == "20-29" &
+                                                                                   Gender == "m" &
+                                                                                   Dietary.Habits == "unhealthy" &
+                                                                                   Bedtime == "01:15" &
+                                                                                   Sleep.Disorders == "yes"
+))
+predicted
